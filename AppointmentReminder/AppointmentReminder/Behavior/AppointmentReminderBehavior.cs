51a2a8bce04a469cfb2af380c1b1a0adbc85ad16
ganeshamoorthy.chandramoorthy <ganeshamoorthy.chandramoorthy@syncfusion.com>
2020-12-22 11:56:00 +0530

    Updated appointment reminder sample

--- AppointmentReminder/AppointmentReminder/Behavior/AppointmentReminderBehavior.cs
+++ AppointmentReminder/AppointmentReminder/Behavior/AppointmentReminderBehavior.cs
@@ -26,9 +26,9 @@ namespace AppointmentReminder
                 {
                     Outlook.Application application = new Outlook.Application();
                     Outlook.MailItem eMail = (Outlook.MailItem)application.CreateItem(Outlook.OlItemType.olMailItem);
-                    eMail.Subject = reminder.Appointment.Subject + " - " + reminder.Appointment.ActualStartTime.ToString();
+                    eMail.Subject = reminder.Appointment.Subject + " reminder alert";
                     eMail.To = "abc@outlook.com";
-                    eMail.Body = reminder.Appointment.Notes;
+                    eMail.Body = reminder.Appointment.Subject + " - " + reminder.Appointment.ActualStartTime.ToString();
                     eMail.Importance = Outlook.OlImportance.olImportanceNormal;
                     eMail.Send();
                 }
